3d1e4c09cb99f50ac26dc3e419a31606273ad84f
USER-PC\User <artem.voronov90@gmail.com>
2018-04-06 12:08:21 +0300

    Add 2d dice sprite

---
+++ SampleGitProject/Assets/MobileController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileController : MonoBehaviour {
+
+    public float speed = 2.0f;
+    //public Transform thisTransform;
+
+
+    //private void Awake()
+    //{
+    //    thisTransform = GetComponent<Transform>();
+    //}
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position += new Vector3(Input.acceleration.x * speed * Time.deltaTime,
+                                          0,
+                                          -Input.acceleration.z * speed * Time.deltaTime);
+
+        Debug.Log(Input.acceleration);
+	}
+}
